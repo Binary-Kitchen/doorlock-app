bdbe5ff088b00fbe080fec6ab5f46567bdc47cdc
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-04-03 19:31:24 +0200

    Add version to about screen
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- app/src/main/java/kitchen/binary/kitchendoorlock/About.java
+++ app/src/main/java/kitchen/binary/kitchendoorlock/About.java
@@ -1,19 +1,26 @@
 package kitchen.binary.kitchendoorlock;
 
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class About extends ActionBarActivity {
+
+    TextView versionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        versionText = (TextView) findViewById(R.id.versionText);
+        versionText.setText("Version: " + BuildConfig.VERSION_NAME);
     }
 
     public void onBack(View view)
