8c95fb86c535805e006162dec8e7587370fb4c9d
Ralf Ramsauer <ralf@binary-kitchen.de>
2018-03-21 16:39:29 +0100

    Delete old stuff
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- README.md
+++
@@ -1,1 +0,0 @@
-# doorlock-app
--- app/src/main/java/kitchen/binary/kitchendoorlock/Answer.java
+++
@@ -1,8 +0,0 @@
-package kitchen.binary.kitchendoorlock;
-
-public class Answer {
-    public enum Sound {Success, Fail, None}
-
-    public String message;
-    public Sound sound;
-}
