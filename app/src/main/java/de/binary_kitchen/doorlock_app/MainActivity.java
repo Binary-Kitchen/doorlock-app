d3a047e3905ec0b559b668b36d4ae0c3bc137f2a
Thomas Schmid <thomas.schmid111@web.de>
2018-06-29 23:57:16 +0200

    Adding a toolbar to the main activity
    Signed-off-by: Thomas Schmid <thomas.schmid111@web.de>

--- app/src/main/java/de/binary_kitchen/doorlock_app/MainActivity.java
+++ app/src/main/java/de/binary_kitchen/doorlock_app/MainActivity.java
@@ -2,6 +2,7 @@ package de.binary_kitchen.doorlock_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
     }
 }
