97825d7980a7fac7bbbd96bc8ac1c80ffb8b227a
Ralf Ramsauer <ralf@binary-kitchen.de>
2018-08-20 00:30:45 +0200

    Boil down DoorlockAPI
    Remove unneeded routines and open code single user functions.
    
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- app/src/main/java/de/binary_kitchen/doorlock_app/doorlock_api/DoorlockApi.java
+++ app/src/main/java/de/binary_kitchen/doorlock_app/doorlock_api/DoorlockApi.java
@@ -38,64 +38,16 @@ public class DoorlockApi {
         this.ctx = ctx;
     }
 
-    private Request buildCommandRequest(ApiCommand command, String user, String password, String target){
-        RequestBody requestBody = new FormBody.Builder()
-                .add("command", command.toString())
-                .add("target",target)
-                .add("user",user)
-                .add("pass",password)
-                .build();
-        return new Request.Builder()
-                .url(baseUrl+"api")
-                .post(requestBody)
-                .build();
-    }
-
     public void issueCommand(ApiCommand command){
         OkHttpClient client = new OkHttpClient();
-        client.newCall(buildCommandRequest(command, username, password, target))
-                .enqueue(commandCallback);
-    }
+        RequestBody requestBody = new FormBody.Builder()
+                .add("command", command.toString())
+                .add("target", target)
+                .add("user", username)
+                .add("pass", password)
+                .build();
+        Request request = new Request.Builder().url(baseUrl + "api").post(requestBody).build();
 
-    private static OkHttpClient getUnsafeOkHttpClient() {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            final TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain,
-                                                       String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain,
-                                                       String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new X509Certificate[0];
-                        }
-                    }
-            };
-
-            // Install the all-trusting trust manager
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            // Create an ssl socket factory with our all-trusting manager
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-            return new OkHttpClient.Builder()
-                    .sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0])
-                    .hostnameVerifier(new HostnameVerifier() {
-                        @Override
-                        public boolean verify(String hostname, SSLSession session) {
-                            return true;
-                        }
-                    }).build();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        client.newCall(request).enqueue(commandCallback);
     }
 }
