54da457e0e1f0b2ebf6008b342859794bbd2197b
Ralf Ramsauer <ralf@binary-kitchen.de>
2018-08-20 00:44:16 +0200

    Remove emulator option
    This is not an application that should be tested in a virtual env.
    
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- app/src/main/java/de/binary_kitchen/doorlock_app/MainActivity.java
+++ app/src/main/java/de/binary_kitchen/doorlock_app/MainActivity.java
@@ -278,20 +278,7 @@ public class MainActivity extends AppCompatActivity {
     {
         return ssid != null
                 && (ssid.equals("\"legacy.binary-kitchen.de\"")
-                || ssid.equals("\"secure.binary-kitchen.de\"")
-                || isEmulator());
-    }
-
-    public static boolean isEmulator()
-    {
-        return Build.FINGERPRINT.startsWith("generic")
-                || Build.FINGERPRINT.startsWith("unknown")
-                || Build.MODEL.contains("google_sdk")
-                || Build.MODEL.contains("Emulator")
-                || Build.MODEL.contains("Android SDK built for x86")
-                || Build.MANUFACTURER.contains("Genymotion")
-                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-                || "google_sdk".equals(Build.PRODUCT);
+                || ssid.equals("\"secure.binary-kitchen.de\""));
     }
 
     boolean has_wifi_permissions()
