48f328d24f3c20974fa8ae9a4af3ab13d183fda9
Ralf Ramsauer <ralf@binary-kitchen.de>
2018-08-28 23:42:19 +0200

    remove superfluous routines and assignments
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- app/src/main/java/de/binary_kitchen/doorlock_app/doorlock_api/ApiErrorCode.java
+++ app/src/main/java/de/binary_kitchen/doorlock_app/doorlock_api/ApiErrorCode.java
@@ -2,7 +2,7 @@ package de.binary_kitchen.doorlock_app.doorlock_api;
 
 import com.google.gson.annotations.SerializedName;
 
-public enum ApiErrorCode{
+public enum ApiErrorCode {
     @SerializedName("0") SUCCESS(0),
     @SerializedName("1") PERMISSION_DENIED(1),
     @SerializedName("2") ALREADY_LOCKED(2),
@@ -10,12 +10,7 @@ public enum ApiErrorCode{
     @SerializedName("4") INVALID(4),
     @SerializedName("5") LDAP_ERROR(5);
 
-    final private int value;
-
-    ApiErrorCode(int value){
-        this.value=value;
-    }
-    public int getValue(){
-        return this.value;
+    ApiErrorCode(int value)
+    {
     }
 }
