Binary-Kitchen/doorlock-app

commit 70793d43fdccee58f1ed33018055a8d4e748af26

Allow to specify Space API URL
Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

app/src/main/java/de/binary_kitchen/doorlock_app/SpaceWidget.java
@@ -19,6 +19,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.widget.RemoteViews;
 
@@ -35,8 +37,6 @@ import okhttp3.Response;
 
 
 public class SpaceWidget extends AppWidgetProvider {
-    private final Request.Builder request_uri =
-            new Request.Builder().url("https://www.binary-kitchen.de/spaceapi.php");
     private final OkHttpClient client = new OkHttpClient();
     private static boolean open, state_valid;
 
@@ -72,7 +72,14 @@ public class SpaceWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences prefs;
+        String url_spaceapi;
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        url_spaceapi = prefs.getString("spaceapi", "");
+
         SpaceAPICallback spaceAPICallback = new SpaceAPICallback(context);
+        Request.Builder request_uri = new Request.Builder().url(url_spaceapi);
         Request request = request_uri.get().build();
         client.newCall(request).enqueue(spaceAPICallback);
     }

app/src/main/res/values/strings.xml
@@ -35,4 +35,5 @@
     <string name="closed">Closed</string>
     <string name="err_location_provider">WiFi switching temporarily disabled due to missing location services</string>
     <string name="present">Somebody\'s inside</string>
+    <string name="spaceapi">Space API URL</string>
 </resources>

app/src/main/res/xml/app_preferences.xml
@@ -44,6 +44,13 @@
             android:inputType="text"
             android:title="@string/hostname_title"
             android:defaultValue="lock.binary.kitchen" />
+        <EditTextPreference
+            android:key="spaceapi"
+            android:selectAllOnFocus="true"
+            android:singleLine="true"
+            android:inputType="text"
+            android:title="@string/spaceapi"
+            android:defaultValue="https://www.binary-kitchen.de/spaceapi.php" />
     </PreferenceCategory>
     <PreferenceCategory android:title="@string/category_information">
         <Preference

commit fcbfe518d6898ce37d139b7d9fb870dbfd1292c2

HOTFIX: Move spaceapi URL to R string resources
We had an update fuckup... In 70793d43f ("Allow to specify Space API URL"), we
allow users to specify their own spaceapi URL. This triggers a bug, if the
users directly updates from 2.2 to 2.3: When the app starts, the setting is
missing. It is written to the SharedPreference storage once the settings screen
is opened. Before that, we will get an empty string and the App will crash, if
a widget is active, as it tries to parse an empty URL.

Fix this by falling back to an R resource if we read an empty string.

Fixes: 70793d43f ("Allow to specify Space API URL")
Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

app/src/main/java/de/binary_kitchen/doorlock_app/SpaceWidget.java
@@ -77,6 +77,8 @@ public class SpaceWidget extends AppWidgetProvider {
 
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
         url_spaceapi = prefs.getString("spaceapi", "");
+        if (url_spaceapi.isEmpty())
+            url_spaceapi = context.getResources().getString(R.string.default_spaceapi);
 
         SpaceAPICallback spaceAPICallback = new SpaceAPICallback(context);
         Request.Builder request_uri = new Request.Builder().url(url_spaceapi);

app/src/main/res/values/strings.xml
@@ -36,4 +36,5 @@
     <string name="err_location_provider">WiFi switching temporarily disabled due to missing location services</string>
     <string name="present">Somebody\'s inside</string>
     <string name="spaceapi">Space API URL</string>
+    <string name="default_spaceapi" translatable="false">https://www.binary-kitchen.de/spaceapi.php</string>
 </resources>

app/src/main/res/xml/app_preferences.xml
@@ -50,7 +50,7 @@
             android:singleLine="true"
             android:inputType="text"
             android:title="@string/spaceapi"
-            android:defaultValue="https://www.binary-kitchen.de/spaceapi.php" />
+            android:defaultValue="@string/default_spaceapi" />
     </PreferenceCategory>
     <PreferenceCategory android:title="@string/category_information">
         <Preference